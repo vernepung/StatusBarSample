675e338b073836397405b0924bd5e5756704b325
vernepung <vernepung@hotmail.com>
2014-11-23 18:53:27 +0800

    all code with sample

---
+++ StatusBarSample.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上提供
+
+namespace StatusBarSample
+{
+    /// <summary>
+    /// 可独立使用或用于导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+        }
+
+        /// <summary>
+        /// 在此页将要在 Frame 中显示时进行调用。
+        /// </summary>
+        /// <param name="e">描述如何访问此页的事件数据。
+        /// 此参数通常用于配置页。</param>
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: 准备此处显示的页面。
+
+            // TODO: 如果您的应用程序包含多个页面，请确保
+            // 通过注册以下事件来处理硬件“后退”按钮:
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed 事件。
+            // 如果使用由某些模板提供的 NavigationHelper，
+            // 则系统会为您处理该事件。
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StatusBarExtensions.LoadingWithProgress("进度条", async () =>
+            {
+                StatusBarExtensions.ShowMessage("完成");
+            });
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            StatusBarExtensions.ShowMessage("你好");
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            StatusBarExtensions.Loading("正在加载");
+            await System.Threading.Tasks.Task.Delay(5000);
+            StatusBarExtensions.StopLoading();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            StatusBarExtensions.ShowMessage(" ", null, false);
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            StatusBarExtensions.ShowMessage("StatusBarSample", null, false);
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            StatusBarExtensions.ShowMessage("", null, false);
+        }
+
+    }
+}
---
+++ StatusBarSample.WindowsPhone/StatusBarExtensions.cs
@@ -0,0 +1,120 @@
+﻿#region 版 本 注 释
+/****************************************************
+	* 项 目 名: $projectname$
+    * 文 件 名: StatusBarSample.WindowsPhone
+    * Copyright © 2014 Verne Pung
+    * 创 建 人: vernepung
+    * 创建日期: 2014年11月23日 16:20:11
+    ****************************************************** 
+    * 修 改 人：
+    * 修改日期：
+    * 备注描述：
+    *  
+    * 
+*******************************************************/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Core;
+using Windows.UI.ViewManagement;
+
+namespace StatusBarSample
+{
+    public static class StatusBarExtensions
+    {
+        private static bool IsLoading = false;
+        private static StatusBar statusBar
+        {
+            get
+            {
+                return StatusBar.GetForCurrentView();
+            }
+        }
+        private static CoreApplicationView coreApplication
+        {
+            get
+            {
+                return CoreApplication.GetCurrentView();
+            }
+        }
+        public static void LoadingWithProgress(string text, Action completedAction)
+        {
+            coreApplication.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async delegate
+            {
+                Random random = new Random();
+                IsLoading = true;
+                statusBar.ProgressIndicator.ProgressValue = 0;
+
+                statusBar.ProgressIndicator.Text = text;
+                await statusBar.ProgressIndicator.ShowAsync();
+                while (IsLoading)
+                {
+                    await Task.Delay(random.Next(200, 500));
+                    if (IsLoading)
+                    {
+                        if (statusBar.ProgressIndicator.ProgressValue >= 1)
+                        {
+                            IsLoading = false;
+                            break;
+                        }
+                        statusBar.ProgressIndicator.ProgressValue += random.NextDouble() / 10.0;
+                    }
+                    else
+                        break;
+                }
+                if (completedAction != null)
+                    completedAction.Invoke();
+            });
+        }
+        public static void Loading(string text)
+        {
+            coreApplication.Dispatcher.RunAsync(
+                 Windows.UI.Core.CoreDispatcherPriority.Normal,
+                 async delegate
+                 {
+                     IsLoading = true;
+                     statusBar.ProgressIndicator.ProgressValue = null;
+                     statusBar.ProgressIndicator.Text = text;
+                     await statusBar.ProgressIndicator.ShowAsync();
+                 });
+        }
+        public static void StopLoading()
+        {
+            coreApplication.Dispatcher.RunAsync(
+                Windows.UI.Core.CoreDispatcherPriority.Normal,
+                async delegate
+                {
+                    if (IsLoading)
+                    {
+                        await statusBar.ProgressIndicator.HideAsync();
+                        IsLoading = false;
+                        statusBar.ProgressIndicator.ProgressValue = 0;
+                    }
+                });
+        }
+
+        public static void ShowMessage(string text, TimeSpan? timeSpan = null, bool allowHide = true)
+        {
+            CoreApplication.GetCurrentView().Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async delegate
+            {
+                StopLoading();
+
+                statusBar.ProgressIndicator.Text = text;
+                statusBar.ProgressIndicator.ProgressValue = 0;
+                await statusBar.ProgressIndicator.ShowAsync();
+                if (!allowHide) return;
+                if (timeSpan == null)
+                    timeSpan = TimeSpan.FromSeconds(3);
+                await Task.Delay((TimeSpan)timeSpan);
+                await CoreApplication.GetCurrentView().Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, async delegate
+                {
+                    await statusBar.ProgressIndicator.HideAsync();
+                });
+            });
+        }
+    }
+}
